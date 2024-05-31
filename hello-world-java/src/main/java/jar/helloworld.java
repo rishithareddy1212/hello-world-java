f414b54e942fb98f55a88573469acd3b714f31b9
rishi <rishiyeruva78@gmail.com>
2024-05-31 11:33:48 +0530

    modified

--- hello-world-java/src/main/java/jar/helloworld.java
+++ hello-world-java/src/main/java/jar/helloworld.java
@@ -2,6 +2,6 @@ class Test
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program.This is rishi .. pradeep 1today is friday");
+        System.out.println("My First Java Program.This is rishi .. pradeep ambati 1today is friday");
     }
 };
