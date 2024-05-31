f95f0ba44282dd9842c9cb1c4924112f194b45ae
rishi <rishiyeruva78@gmail.com>
2024-05-31 11:26:11 +0530

    code change

--- hello-world-java/src/main/java/jar/helloworld.java
+++ hello-world-java/src/main/java/jar/helloworld.java
@@ -2,6 +2,6 @@ class Test
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program.This is rishi");
+        System.out.println("My First Java Program.This is rishi today is friday");
     }
 };
